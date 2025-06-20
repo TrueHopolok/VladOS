c585b09d0042d0aae6bc70d62d075bdef1c773dd
HOPOLOK <vadim.truhanov@inbox.lv>
2025-06-20 13:38:03 +0300

    Added VOS prefab

---
+++ modules/vos/vos.go
@@ -0,0 +1,6 @@
+// VladOS Security (shortly VOS) is a package used for all security related stuff like JWT, encryption keys and etc.
+//
+// TODO: add better description when as package will be developed.
+package vos
+
+//go:generate go tool github.com/princjef/gomarkdoc/cmd/gomarkdoc -o documentation.md
