53847feb38328b6ad2eca3b35274c5cbea0c265f
HOPOLOK <vadim.truhanov@inbox.lv>
2025-08-08 23:20:52 +0300

    premade+ stats

--- modules/bot/cmd/stats.go
+++ modules/bot/cmd/stats.go
@@ -14,13 +14,30 @@ var CommandStats Command = Command{
 Output all stats for ceratin game.
 With a your placement and top placement in the leaderboard.`,
 	handler: func(ctx *telegohandler.Context, update telego.Update) error {
-		bot, chatID, _, valid, err := utilStart(ctx, update, "stats", 1)
+		bot, chatID, args, valid, err := utilStart(ctx, update, "stats", 1)
 		if !valid {
 			return err
 		}
 
 		_, err = bot.SendMessage(ctx, tu.Message(chatID, "Sorry, but currently this command is in development. Comeback later."))
 		return err
+
+		switch args[0] {
+		case "slot":
+			return nil
+		case "dice":
+			return nil
+		case "bjack":
+			_, err = bot.SendMessage(ctx, tu.Message(chatID, "Sorry, but currently this command is in development. Comeback later."))
+			return err
+		default:
+			_, err = bot.SendMessage(ctx, tu.MessageWithEntities(chatID,
+				tu.Entity("Provided categories does not exists.\nTry any of those:\n"),
+				tu.Entity(" /stats slot\n").Code(),
+				tu.Entity(" /stats dice\n").Code(),
+				tu.Entity(" /stats bjack\n").Code()))
+			return err
+		}
 	},
 	conversation: nil,
 }
