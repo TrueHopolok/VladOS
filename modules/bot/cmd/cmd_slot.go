feb81bce2fdc8b43b4ec68391729658489b21509
HOPOLOK <vadim.truhanov@inbox.lv>
2025-08-08 12:08:10 +0300

    added: slot scoring system

--- modules/bot/cmd/cmd_slot.go
+++ modules/bot/cmd/cmd_slot.go
@@ -8,7 +8,8 @@ import (
 	tu "github.com/mymmrac/telego/telegoutil"
 )
 
-// TODO: finish handler in value
+var fruitWin = []int{}
+
 var CommandSlot Command = Command{
 	InfoBrief: "spin slot machine",
 	InfoFull: `
@@ -30,7 +31,34 @@ Has a leaderboard to count largest winstreak.`,
 		if msg.Dice == nil {
 			return fmt.Errorf("msg is not a dice result: %v", msg)
 		}
-		// msg.Dice.Value
+		// scored := getSlotScore(msg.Dice.Value)
 		return nil
 	},
+}
+
+/*
+Returns a result of a slot machine in this way:
+
+	slot_values = []string{"bar", "grapes", "lemon", "seven"}
+
+Counts amount of each slot and returns an expected score.
+*/
+func getSlotScore(value int) int {
+	value--
+	scores := make([]int, 4)
+	for _ = range 3 {
+		scores[value%4]++
+		value /= 4
+	}
+	if scores[1] == 2 || scores[2] == 2 {
+		return 1
+	} else if scores[3] == 2 {
+		return 3
+	} else if scores[1] == 3 || scores[2] == 3 {
+		return 5
+	} else if scores[3] == 3 {
+		return 10
+	} else {
+		return 0
+	}
 }
