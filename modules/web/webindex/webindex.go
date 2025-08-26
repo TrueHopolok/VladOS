ece96fc4b9c53f241f178a9deead976bb81b8b2c
HOPOLOK <vadim.truhanov@inbox.lv>
2025-08-26 15:11:24 +0300

    fixed tmls logic

--- modules/web/webindex/webindex.go
+++ modules/web/webindex/webindex.go
@@ -11,7 +11,7 @@ import (
 
 func TodoHandler(w http.ResponseWriter, r *http.Request) {
 	slog.Debug("http req", "mtd", r.Method, "url", r.URL, "handler", "index-todo")
-	err := webtmls.Tmls.ExecuteTemplate(w, "base.html", nil)
+	err := webtmls.Tmls.ExecuteTemplate(w, "update.html", nil)
 	if err != nil {
 		slog.Warn("http req", "mtd", r.Method, "url", r.URL, "error", err)
 	}
