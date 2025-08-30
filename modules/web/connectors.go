d2ae9ddfc2267a3f3608bd8a4af5efeec03660ae
HOPOLOK <vadim.truhanov@inbox.lv>
2025-08-30 14:01:06 +0300

    add: basic todo http handlers

--- modules/web/connectors.go
+++ modules/web/connectors.go
@@ -6,6 +6,8 @@ import (
 	"github.com/TrueHopolok/VladOS/modules/cfg"
 	"github.com/TrueHopolok/VladOS/modules/vos"
 	"github.com/TrueHopolok/VladOS/modules/web/webindex"
+	"github.com/TrueHopolok/VladOS/modules/web/webleaderboard"
+	"github.com/TrueHopolok/VladOS/modules/web/websuggestions"
 )
 
 // Connects to [net/http.ServeMux] handler functions with
@@ -13,6 +15,8 @@ import (
 // for the function [github.com/TrueHopolok/VladOS/modules/vos.AuthMiddleware].
 func ConnectEveryone(mux *http.ServeMux) {
 	mux.HandleFunc("GET /", vos.AuthMiddlewareFunc(webindex.Handle, vos.Everyone))
+	mux.HandleFunc("GET /leaderboard/", vos.AuthMiddlewareFunc(webleaderboard.Handle, vos.Everyone))
+	mux.HandleFunc("GET /suggestions/", vos.AuthMiddlewareFunc(websuggestions.Handle, vos.Everyone))
 }
 
 // Connects to [net/http.ServeMux] handler functions with
@@ -32,7 +36,10 @@ func ConnectUnauthorized(mux *http.ServeMux) {
 // Connects to [net/http.ServeMux] handler functions to server static files
 // on the [github.com/TrueHopolok/VladOS/modules/cfg.Config.WebStaticDir] path.
 func ConnectFileHandlers(mux *http.ServeMux) {
-	mux.Handle("GET /static/", http.StripPrefix("/static/", http.FileServer(http.Dir(cfg.Get().WebStaticDir))))
+	fs := http.FileServer(http.Dir(cfg.Get().WebStaticDir))
+	mux.Handle("GET /static/", http.StripPrefix("/static/", fs))
+	mux.Handle("GET /leaderboard/static/", http.StripPrefix("/leaderboard/static/", fs))
+	mux.Handle("GET /suggestions/static/", http.StripPrefix("/suggestions/static/", fs))
 	mux.HandleFunc("GET /favicon.ico", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./static/favicon.png")
 	})
