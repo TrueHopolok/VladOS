b3d33ded10e16808fe546d328f9e8d3b23470dc5
TrueHopolok <vadim.truhanov@inbox.lv>
2025-10-06 15:33:40 +0300

    build settings updated

--- build.sh
+++ build.sh
@@ -7,10 +7,13 @@ touch build/run.sh
 echo "./vlados.exe -config=configs/release.cfg" > build/run.sh
 chmod 777 build/run.sh 
 
+touch build/README.md
+echo "To launch: execute **run.sh** script." >> build/README.md
+echo "Required a **bot.key** file in **./configs/** directory with a bot's telegram API key to launch." >> build/README.md
+
 cp -r static/ build/static/
 
 mkdir build/configs/
 cp configs/release.cfg build/configs/release.cfg
-cp configs/bot.key build/configs/bot.key
 
 zip -r build/vlados.zip build
